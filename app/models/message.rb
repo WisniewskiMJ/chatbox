839c396ae2291e0d04b2a82139937eb9cf14ab20
Bakshee <bakshee.jw@gmail.com>
2021-07-08 18:54:03 +0200

    Create message model

---
+++ app/models/message.rb
@@ -0,0 +1,6 @@
+class Message < ApplicationRecord
+  validates :user_id, presence: true
+  validates :content, presence: true
+
+  belongs_to :user
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_08_112154) do
+ActiveRecord::Schema.define(version: 2021_07_08_164115) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "messages", force: :cascade do |t|
+    t.text "content", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
